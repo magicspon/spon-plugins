cc5c1142eec1849790984c1c60ce6f13b3cf5a77
Dave Stockley <magicspon@gmail.com>
2019-06-23 23:28:21 +0100

    add withPlugins function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,31 @@ export { default as inview } from './plugins/inview'
 export { default as mutation } from './plugins/mutation'
 export { default as resize } from './plugins/resize'
 export { default as scroll } from './plugins/scroll'
+import { registerPlugins } from '@spon/core'
+
+export function withPlugins(...plugins) {
+	/**
+	 * @param {function} module the module to bind to
+	 * @memberOf connect
+	 * @return {function}
+	 */
+	return module => {
+		return ({ name, ...props }) =>
+			module({
+				...props,
+				name,
+				plugins: {
+					...plugins.reduce(
+						(acc, curr) => ({
+							...acc,
+							...curr({
+								register: registerPlugins(name),
+								...props
+							})
+						}),
+						{}
+					)
+				}
+			})
+	}
+}
